f18269f72227e88978e090ad6c35a1dcabb97333
Krzysztof M <marecki.krzysztof@gmail.com>
2016-12-04 23:39:17 +0100

    0.0.3 Add ResponseUtil class

--- src/express-app/expressApp.ts
+++ src/express-app/expressApp.ts
@@ -1,3 +1,7 @@
+import {ResponseUtil} from './utils/response';
+
 export class ExpressApp {
     static physicalPath: string;
+
+    static response: ResponseUtil;
 }
---
+++ src/express-app/utils/response.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+
+export class ResponseUtil {
+
+    static handleError(
+        res: express.Response,
+        message: string,
+        reason: string,
+        code?: number): void {
+        console.log('ERROR: ' + reason);
+        res.status(code || 500).json({ 'error': message });
+    }
+}
